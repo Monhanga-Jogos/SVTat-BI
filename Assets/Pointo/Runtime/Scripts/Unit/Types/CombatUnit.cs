a1d7ed059cdcd48240889dddbb6130dacf675f0e
Cap Carlos Matos <carlosmatos.batista@eb.mil.br>
2023-09-01 00:29:34 -0300

    corrigido possibilidade de movimentar ou dar comandos a frações destruídas

--- Assets/Pointo/Runtime/Scripts/Unit/Types/CombatUnit.cs
+++ Assets/Pointo/Runtime/Scripts/Unit/Types/CombatUnit.cs
@@ -37,6 +37,7 @@ namespace Pointo.Unit
             {
                 UnitTargetHandler.currentState = UnitTargetHandler.UnitState.Destroyed;
                 GetComponent<MeshRenderer>().material = unitSo.destroyedMat;
+                GetComponent<BoxCollider>().enabled = false;
             }
         }
         private void HandleObjectClicked(GameObject targetObject)
